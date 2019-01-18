becc59e34653d6fd7ba729f73371db8a56f2d75a
Pranav R Prasad <pranavrp.pr@gmail.com>
2019-01-18 18:00:04 +0530

    Add files via upload

---
+++ guesstheno.py
@@ -0,0 +1,23 @@
+import random
+
+noofguesses=1
+no=random.randrange(1,100000)
+
+while noofguesses<16:
+	guess=raw_input("Take a guess ")
+	guess=int(guess)
+	noofguesses=noofguesses+1
+	guessesleft=16-noofguesses
+	print "Guesses left = ", guessesleft
+	if guess<no:
+		guessesleft=str(guessesleft)
+		print "Your guess is low"
+	elif guess>no:
+		guessesleft=str(guessesleft)
+		print "Your guess is high"
+	else:
+		noofguesses=str(noofguesses)
+		print "Your guess is right"
+		exit(0)
+
+print "Game over. You lost. Number = ", no
